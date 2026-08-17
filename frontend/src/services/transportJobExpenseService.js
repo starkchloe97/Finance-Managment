f8b6f0843d690fcfd85949654d21a65dd4562cf6
starkchloe97 <starkchloe97@gmail.com>
2026-08-17 19:27:43 +0500

    Add unexpected cost update API service

--- frontend/src/services/transportJobExpenseService.js
+++ frontend/src/services/transportJobExpenseService.js
@@ -3,5 +3,8 @@ import api from "@/api/axios";
 export const addExpense = (jobId, data) =>
     api.post(`/jobs/${jobId}/expenses`, data);
 
+export const updateExpense = (id, data) =>
+    api.put(`/expenses/${id}`, data);
+
 export const deleteExpense = (id) =>
     api.delete(`/expenses/${id}`);
